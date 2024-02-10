eaa15f0c221d97dd7d2887784dfebe916e4d195c
Matheus Carvalho <matheucar186@gmail.com>
2024-02-10 13:31:12 -0300

    ex 1

---
+++ A001.py
@@ -0,0 +1,9 @@
+# Description pt-br/en
+# Faça um programa que leia dois números inteiros e imprima a soma deles.
+# Write a program that reads two integers and prints their sum.
+a = int(input()) 
+b = int(input())
+
+res = a+b
+
+print(res)
